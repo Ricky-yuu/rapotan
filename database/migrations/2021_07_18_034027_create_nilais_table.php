74ed355669b626a07d168d9ed5be53fe528e194f
Ricky-yuu <meliodaskirisami@gmail.com>
2021-07-18 11:06:02 +0700

    first commit

---
+++ database/migrations/2021_07_18_034027_create_nilais_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNilaisTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('nilais', function (Blueprint $table) {
+            $table->id();
+            $table->string('id_user');
+            $table->string('id_mapel');
+            $table->string('nilai');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('nilais');
+    }
+}
