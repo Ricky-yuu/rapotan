48f10286a1c5e11e20372f180255cd77f0d0e89c
Ricky-yuu <meliodaskirisami@gmail.com>
2021-07-23 03:15:41 +0700

    menambahkan images di frontend

--- resources/views/admin/datanilaikelas.blade.php
+++ resources/views/admin/datanilaikelas.blade.php
@@ -6,7 +6,7 @@
   <div class="col-md-4">
     <a href="{{route('nilaisiswa', ['id' => $kelases->id])}}">
       <div class="card text-center" style="width: 18rem;">
-        <img src="{{asset('img/download.png')}}" class="card-img-top" alt="..." width="100" height="100">
+        <img src="{{asset('images')}}/book.png" class="card-img-top" alt="..." width="100" height="100">
         <div class="card-body">
           <h5 class="card-title">{{$kelases->nama_kelas}}</h5>
         </div>
--- resources/views/admin/kelas.blade.php
+++ resources/views/admin/kelas.blade.php
@@ -10,7 +10,7 @@
             <div class="col mb-4 cardss">
                 <div class="card">
                     <a href="{{route('indexinputnilai')}}" class="text-color-a">
-                        <img src="" class="card-img-top mx-auto d-block" alt="..." style="width:100%; height:100%;">
+                        <img src="{{asset('images')}}/book.png" class="card-img-top mx-auto d-block" alt="..." style="width:100%; height:100%;">
                         <div class="card-body">
                             <h5 class="card-title">{{ $kelas->kelas }}</h5>
                         </div>
