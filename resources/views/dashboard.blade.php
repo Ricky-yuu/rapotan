61f2608048ce787729813bee2a525af4788db419
Ricky-yuu <meliodaskirisami@gmail.com>
2021-07-23 03:35:05 +0700

    lanjutkan lagi nanti

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,11 @@
+
+@extends('layouts.app')
+
+@section('content')
+
+
+<h1> Belum Menemukan Ide Cuy Tidur Dulu</h1>
+
+    
+
+@endsection
--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,6 +1,6 @@
 <ul class="sidebar-menu" data-widget="tree">
   <li class="header">MAIN NAVIGATION</li>
-  <li><a href="https://adminlte.io/docs"><i class="fa fa-book"></i> <span>Dashboard</span></a></li>
+  <li><a href="{{ route('dashboard')}}"><i class="fa fa-book"></i> <span>Dashboard</span></a></li>
   @if(Auth::user()->role == "admin")
   <li><a href="{{ route('menampilkankelas')}}"><i class="fa fa-book"></i> <span>Data Siswa</span></a></li>
   <li><a href="{{ route('indexguruadmin')}}"><i class="fa fa-book"></i> <span>Data Guru</span></a></li>
