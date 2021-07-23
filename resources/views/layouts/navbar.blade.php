6609a90ceba195f4062ec90ab9c6ce1821accd46
Ricky-yuu <meliodaskirisami@gmail.com>
2021-07-23 07:36:06 +0700

    Menambah menu aktif

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,12 +1,13 @@
 <ul class="sidebar-menu" data-widget="tree">
   <li class="header">MAIN NAVIGATION</li>
-  <li><a href="{{ route('dashboard')}}"><i class="fa fa-book"></i> <span>Dashboard</span></a></li>
+
+  <li class="{{ request()->is('dashboard') ? 'active' : '' }}"> ><a href="{{ route('dashboard')}}"><i class="fa fa-book"></i> <span>Dashboard</span></a></li>
   @if(Auth::user()->role == "admin")
-  <li><a href="{{ route('menampilkankelas')}}"><i class="fa fa-book"></i> <span>Data Siswa</span></a></li>
-  <li><a href="{{ route('indexguruadmin')}}"><i class="fa fa-book"></i> <span>Data Guru</span></a></li>
-  <li><a href="{{route ('menampilkannilaikelas')}}"><i class="fa fa-book"></i> <span>Data Nilai</span></a></li>
+  <li class="{{ request()->is('kelas') ? 'active' : '' }}"><a href="{{ route('menampilkankelas')}}"><i class="fa fa-book"></i> <span>Data Siswa</span></a></li>
+  <li class="{{ request()->is('dataguru') ? 'active' : '' }}"><a href="{{ route('indexguruadmin')}}"><i class="fa fa-book"></i> <span>Data Guru</span></a></li>
+  <li class="{{ request()->is('nilaikelas') ? 'active' : '' }}"><a href="{{route ('menampilkannilaikelas')}}"><i class="fa fa-book"></i> <span>Data Nilai</span></a></li>
   @elseif(Auth::user()->role == "guru")
-  <li><a href="{{route ('dataMapel') }}"><i class="fa fa-book"></i> <span>Input Nilai</span></a></li>
+  <li class="{{ request()->is('mapel') ? 'active' : '' }}"><a href="{{route ('dataMapel') }}"><i class="fa fa-book"></i> <span>Input Nilai</span></a></li>
   
   @endif
 
